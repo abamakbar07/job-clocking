e13edc92df1d9d624a06cd0f66199216c7fa9a94
Muhamad Afriansyah - DSV <muhamad.afriansyah@dsv.com>
2025-02-27 15:05:38 +0700

    First commit

---
+++ jobStop.js
@@ -0,0 +1,11 @@
+fetch("http://rpt.apac.dsv.com:81/api/JobClocking/UpdateobClocking", {
+    "headers": {
+      "accept": "application/json, text/plain, */*",
+      "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+      "content-type": "application/json",
+      "Referer": "http://jobclocking.apac.dsv.com/",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": "{\"end_time\":\"2/27/2025 14:41:38\",\"job_clocking_id\":5887341}",
+    "method": "POST"
+  });
